9626d25051f9fc846a775a2d30247c437bb8e0f2
Mihael Konjevic <konjevic@gmail.com>
2015-12-04 14:00:29 +0100

    Make carousel responsive

--- carousel/bits_carousel/bits_carousel.js
+++ carousel/bits_carousel/bits_carousel.js
@@ -5,8 +5,11 @@ import BitModel from "models/bit";
 import "./bits_carousel.less!";
 import "can/map/define/";
 
-var CARD_WIDTH = 350;
+var MIN_CARD_WIDTH = 300;
 
+var getNextClickTS = function(factor){
+	return (new Date()).getTime() + (factor || 5000);
+};
 
 export default can.Component.extend({
 	tag: 'bh-bits-carousel',
@@ -17,6 +20,8 @@ export default can.Component.extend({
 		isLoading: false,
 		fromLeft: 0,
 		carouselWidth: 0,
+		cardWidth: 300,
+		nextClickTimeout : null,
 		define : {
 			params : {
 				value : function(){
@@ -25,7 +30,7 @@ export default can.Component.extend({
 				get : function(lastSetValue){
 					lastSetValue = lastSetValue || new can.Map();
 					lastSetValue.attr('offset', lastSetValue.offset || this.attr('bits').length || 0);
-					lastSetValue.attr('limit', 30);
+					lastSetValue.attr('limit', 5);
 					return lastSetValue;
 				}
 			},
@@ -40,7 +45,10 @@ export default can.Component.extend({
 		},
 		loadNextPage : function(){
 			var self = this;
+			var bits = self.attr('bits');
+			var hasContents = !!bits.length;
 			var params;
+
 
 			if(this.attr('hasNextPage')){
 				this.attr('isLoading', true);
@@ -49,49 +57,77 @@ export default can.Component.extend({
 				//return;
 				this.getBitModel().findAll(params).then(function(data){
 					can.batch.start();
-					
-					if(data.length < self.attr('params.limit')){
-						self.attr('hasNextPage', false);
-					}
 
-					self.attr('params.offset', self.attr('params.offset') + data.length);
-					self.attr('isLoading', false);
-					self.attr('bits').push.apply(self.attr('bits'), data);
-					
+					if(data.length){
+						if(data.length < self.attr('params.limit')){
+							self.attr('hasNextPage', false);
+						}
+						
+						self.attr('params.offset', self.attr('params.offset') + data.length);
+						self.attr('isLoading', false);
+						bits.push.apply(bits, data);
+					} else {
+						self.attr({
+							isLoading: false,
+							hasNextPage: false
+						});
+						bits.push.apply(bits, bits);
+					}
 					can.batch.stop();
+					if(hasContents){
+						self.carouselNext();
+					}
 				}, function(){
 					self.attr({
 						isLoading: false,
 						hasNextPage: false
 					});
 				});
+			} else {
+				bits.push.apply(bits, bits.attr());
+				this.carouselNext();
 			}
 		},
+		loadingCardWidth : function(){
+			return this.attr('cardWidth') - 5;
+		},
 		carouselContentWidth : function(){
-			var width = this.attr('bits').attr('length') * CARD_WIDTH;
+			var cardWidth = this.attr('cardWidth');
+			var width = this.attr('bits').attr('length') * cardWidth;
 			if(this.attr('isLoading')){
-				width = width + CARD_WIDTH;
+				width = width + cardWidth;
 			}
 			return width;
 		},
 		carouselNext : function(ctx, el){
-			el.trigger('interaction:carousel-scroll', [this.attr('state.hubId')]);
+			var cardWidth = this.attr('cardWidth');
+			var factor;
+			if(el){
+				el.trigger('interaction:carousel-scroll', [this.attr('state.hubId')]);
+				factor = 10000;
+			}
+			this.nextClickTimeout = getNextClickTS(factor);
 			if(this.attr('isLoading')){
 				return;
 			}
 
-			var next = this.attr('fromLeft') + CARD_WIDTH;
+			var next = this.attr('fromLeft') + cardWidth;
 			if(next > this.maxFromLeft()){
 				this.loadNextPage();
-				next = next + CARD_WIDTH;
 			}
 			this.attr('fromLeft', Math.min(next, this.maxFromLeft()));
 		},
 		maxFromLeft : function(){
 			return this.carouselContentWidth() - this.attr('carouselWidth') - 10;
 		},
-		carouselPrev : function(){
-			this.attr('fromLeft', Math.max(0, this.attr('fromLeft') - CARD_WIDTH));
+		carouselPrev : function(ctx, el){
+			var cardWidth = this.attr('cardWidth');
+			var factor;
+			if(el){
+				factor = 10000;
+			}
+			this.nextClickTimeout = getNextClickTS(factor);
+			this.attr('fromLeft', Math.max(0, this.attr('fromLeft') - cardWidth));
 		}
 	},
 	helpers : {
@@ -104,14 +140,60 @@ export default can.Component.extend({
 	events : {
 		init: function(){
 			this.measureWidth();
+			this.scope.nextClickTimeout = getNextClickTS();
+			this.resizeIframe();
+		},
+		autoClick : function(){
+			return;
+			var currentTS = (new Date()).getTime();
+			var diff = Math.abs(currentTS - this.scope.nextClickTimeout);
+			if(diff < 100){
+				this.element.find('[can-click="carouselNext"]').click();
+				this.scope.nextClickTimeout = getNextClickTS();
+			}
+			setTimeout(this.autoClick.bind(this), 100);
+		},
+		"{scope} isLoading" : "resizeIframe",
+		"{scope.bits} length" : function(){
+			if(!this.__autoClickInited){
+				this.autoClick();
+				this.__autoClickInited = true;
+			}
+			this.resizeIframe();
 		},
 		'{window} resize': 'measureWidth',
 		measureWidth: function(){
 			var self = this;
+
+
 			clearTimeout(this.__measureWidthTimeout);
 			this.__measureWidthTimeout = setTimeout(function(){
-				self.scope.attr('carouselWidth', self.element.find('.outer-carousel-wrap').width());
+				var outerWidth = self.element.find('.outer-carousel-wrap').width();
+				var cardCount = Math.floor(outerWidth / MIN_CARD_WIDTH) || 1;
+				var cardWidth = Math.max(MIN_CARD_WIDTH, (outerWidth + 10) / cardCount);
+				self.scope.attr({
+					carouselWidth: outerWidth,
+					cardWidth: cardWidth,
+					fromLeft: 0
+				});
+				self.resizeIframe();
 			});
+		},
+		resizeIframe : function(){
+			var self = this;
+			setTimeout(function(){
+				if(!self.element){
+					return;
+				}
+				var maxHeight = 0;
+
+				self.element.find('.outer-card-wrap, .loading-wrap').each(function(){
+					if ($(this).height() > maxHeight) { maxHeight = $(this).outerHeight(); }
+				});
+			
+				self.element.trigger('cardExpanded', maxHeight);
+			});
+			
 		}
 	}
 
--- carousel/embed.js
+++ carousel/embed.js
@@ -1,20 +1,28 @@
 var link = document.getElementById('wowbao-be-social');
 
+var wrapper = document.createElement('div');
+wrapper.className = 'wowbao-bithub-wrapper';
+wrapper.style.maxWidth = '1090px';
+wrapper.style.marginLeft = 'auto';
+wrapper.style.marginRight = 'auto';
+
 var iframe = document.createElement('iframe');
 
 iframe.style.height = '594px';
-iframe.style.width = '1090px';
+iframe.style.width = '100%';
 iframe.style.marginLeft = 'auto';
 iframe.style.marginRight = 'auto';
 iframe.style.marginTop = '50px';
 iframe.style.marginBottom = '50px';
 iframe.style.border = 'none';
 iframe.style.display = 'block';
-iframe.src = "http://wowbao.bithub.com/carousel/embed.html";
+//iframe.src = "http://wowbao.bithub.com/carousel/embed.html";
 
-//iframe.src = "../embed.html";
+iframe.src = "../embed.html";
 
-link.parentNode.insertBefore(iframe, link);
+wrapper.appendChild(iframe);
+
+link.parentNode.insertBefore(wrapper, link);
 link.parentNode.removeChild(link);
 
 window.addEventListener('message', function(ev){
