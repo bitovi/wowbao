df07672b6336482504dcc2b9f966ec8d748e56bc
Mihael Konjevic <konjevic@gmail.com>
2016-01-12 17:59:50 +0100

    Carousel is responsive in iOS

--- carousel/embed.js
+++ carousel/embed.js
@@ -2,14 +2,17 @@ var link = document.getElementById('wowbao-be-social');
 
 var wrapper = document.createElement('div');
 wrapper.className = 'wowbao-bithub-wrapper';
-wrapper.style.maxWidth = '1090px';
+wrapper.style.maxWidth = '100%';
 wrapper.style.marginLeft = 'auto';
 wrapper.style.marginRight = 'auto';
+wrapper.style.paddingLeft = '15px';
+wrapper.style.paddingRight = '15px';
 
 var iframe = document.createElement('iframe');
 
 iframe.style.height = '594px';
-iframe.style.width = '100%';
+iframe.style.width = '1px';
+iframe.style.minWidth = '100%';
 iframe.style.marginLeft = 'auto';
 iframe.style.marginRight = 'auto';
 iframe.style.marginTop = '50px';
@@ -17,6 +20,7 @@ iframe.style.marginBottom = '50px';
 iframe.style.border = 'none';
 iframe.style.display = 'block';
 iframe.src = "http://wowbao.bithub.com/carousel/embed.html";
+iframe.scrolling = 'no';
 
 //iframe.src = "../embed.html";
 
